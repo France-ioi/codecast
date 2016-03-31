371eb561dfc3f731cd90b8259c3e5d81c47b9545
Sebastien Carlier <s.carlier@epixode.fr>
2016-03-31 22:47:16 +0000

    hide redux dev tools by default

--- frontend/dev_tools.js
+++ frontend/dev_tools.js
@@ -6,6 +6,7 @@ import DockMonitor from 'redux-devtools-dock-monitor';
 
 export default createDevTools(
   <DockMonitor
+      defaultIsVisible={false}
       toggleVisibilityKey='ctrl-h'
       changePositionKey='ctrl-q'
       changeMonitorKey='ctrl-m'>
