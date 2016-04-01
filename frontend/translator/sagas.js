26e5e7b10fe04b833e78a9aa4945a89df3dbab9d
Sebastien Carlier <s.carlier@epixode.fr>
2016-04-01 04:13:29 +0000

    record translate events

--- frontend/translator/sagas.js
+++ frontend/translator/sagas.js
@@ -1,17 +1,23 @@
 
 import {takeLatest} from 'redux-saga';
 import {put, call} from 'redux-saga/effects';
+
 import {asyncRequestJson} from '../api';
+import {recordEventAction} from '../recorder';
 
 export default function (actions) {
 
   function* translateSource (action) {
     const {source} = action;
     try {
+      yield put(recordEventAction(['translate', source]));
       const {ast} = yield call(asyncRequestJson, '/translate', {source});
       yield put({type: actions.translateSourceSucceeded, source, syntaxTree: ast});
+      yield put(recordEventAction(['translateSuccess', ast]));
     } catch (error) {
-      yield put({type: actions.translateSourceFailed, error: error.toString(), source});
+      const message = error.toString();
+      yield put({type: actions.translateSourceFailed, error: message, source});
+      yield put(recordEventAction(['translateFailure', message]));
     }
   }
 
