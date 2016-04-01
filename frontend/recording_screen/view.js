e0977da4d3f08c652d808e76f389f8a92eb91bc6
Sebastien Carlier <s.carlier@epixode.fr>
2016-04-01 04:09:36 +0000

    split editor off recording_screen

--- frontend/recording_screen/view.js
+++ frontend/recording_screen/view.js
@@ -2,72 +2,12 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {Button} from 'react-bootstrap';
-import AceEditor from 'react-ace';
 import EpicComponent from 'epic-component';
-import * as ace from 'brace';
-const Range = ace.acequire('ace/range').Range;
 
 import Terminal from '../terminal';
 import actions from '../actions';
 import {recordEventAction} from '../recorder';
-
-export const Editor = EpicComponent(self => {
-
-  let editor, selection = null;
-
-  const samePosition = function (p1, p2) {
-    return p1.row == p2.row && p1.column == p2.column;
-  };
-
-  const sameSelection = function (s1, s2) {
-    if (typeof s1 !== typeof s2 || !!s1 !== !!s2)
-      return false;
-    return samePosition(s1.start, s2.start) && samePosition(s1.end, s2.end);
-  };
-
-  const setSelection = function (selection_) {
-    if (!editor || sameSelection(selection, selection_))
-      return;
-    selection = selection_;
-    if (selection) {
-      editor.selection.setRange(new Range(
-        selection.start.row, selection.start.column,
-        selection.end.row, selection.end.column));
-    } else {
-      editor.selection.setRange(new Range(0, 0, 0, 0));
-    }
-  };
-
-  const onSelectionChanged = function () {
-    const selection_ = editor.selection.getRange();
-    if (sameSelection(selection, selection_))
-      return;
-    selection = selection_;
-    self.props.onSelect(selection);
-  };
-
-  const onTextChanged = function (edit) {
-    // The callback must not trigger a rendering of the Editor.
-    self.props.onEdit(edit)
-  };
-
-  const onLoad = function (editor_) {
-    editor = editor_;
-    editor.$blockScrolling = Infinity;
-    setSelection(self.props.selection);
-    editor.selection.addEventListener("changeCursor", onSelectionChanged, true);
-    editor.selection.addEventListener("changeSelection", onSelectionChanged, true);
-    editor.session.doc.on("change", onTextChanged, true);
-    editor.focus();
-  };
-
-  self.render = function () {
-    const {name, value, selection, onChange, readOnly} = self.props;
-    setSelection(selection);
-    return <AceEditor mode="c_cpp" theme="github" name={name} value={value} onLoad={onLoad} onChange={onChange} readOnly={readOnly} width='100%' height='336px'/>;
-  };
-
-});
+import {Editor}  from './editor';
 
 export const RecordingScreen = EpicComponent(self => {
 
