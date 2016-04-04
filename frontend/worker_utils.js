c3a99124df0cc4bb690226fbcf36b8c3cab1f32a
Sebastien Carlier <s.carlier@epixode.fr>
2016-04-04 13:11:32 +0000

    clean up worker spawn/kill/call utilites

--- frontend/worker_utils.js
+++ frontend/worker_utils.js
@@ -1,6 +1,6 @@
 
 import EventEmitter from 'eventemitter2';
-import {put, cps} from 'redux-saga/effects';
+import {call, cps} from 'redux-saga/effects';
 
 export function workerUrlFromText (text) {
   const blob = new Blob([text], {type: "application/javascript;charset=UTF-8"});
@@ -8,47 +8,55 @@ export function workerUrlFromText (text) {
   return url;
 }
 
-export function spawnWorker (url) {
+export function* spawnWorker (url) {
+  const worker = yield call(asyncSpawnWorker, url);
+  const emitter = worker.emitter = new EventEmitter();
+  worker.nextResponseId = 1;
+  worker.onmessage = emitResponseEvent;
+  return worker;
+  function emitResponseEvent (event) {
+    emitter.emit(event.data.id, event.data);
+  };
+};
+
+export function* killWorker (worker) {
+  worker.terminate();
+};
+
+export function* callWorker (worker, message) {
+  message.responseId = 'r' + worker.nextResponseId;
+  worker.nextResponseId += 1;
+  worker.postMessage(message);
+  return yield cps(waitForResponse);
+  function waitForResponse (callback) {
+    worker.emitter.once(message.responseId, function (event) {
+      callback(null, event);
+    });
+  }
+};
+
+function asyncSpawnWorker (url) {
   return new Promise(function (resolve, reject) {
     if (typeof window.Worker !== 'function') {
       return reject('Web Worker support is missing');
     }
     const worker = new Worker(url);
     worker.onerror = function (event) {
-      worker.onerror = null;
+        worker.onerror = null;
+        worker.onmessage = null;
       reject('worker failed to initialize');
     };
     worker.onmessage = function (event) {
-      // The first message from the worker is used to indicate that
-      // it finished initializing.
       worker.onerror = null;
       worker.onmessage = null;
-      resolve({worker});
+      // The worker is expected to post a null message once initialized,
+      // any other value is considered to be an error.
+      if (event.data !== null) {
+        return reject(event.data);
+      } else {
+        resolve(worker);
+      }
     };
-    worker.postMessage({type: '__init__'});
+    worker.postMessage(null);
   });
-};
-
-export function* watchWorker (worker, eventType) {
-  const queue = [];
-  const emitter = new EventEmitter();
-  function getNextMessage (callback) {
-    if (queue.length > 0) {
-      callback(null, queue.shift());
-    } else {
-      emitter.once('message', function () {
-        callback(queue.shift());
-      });
-    }
-  }
-  worker.onmessage = function (event) {
-    queue.push(event.data);
-    if (queue.length === 1) {
-      emitter.emit('message');
-    }
-  };
-  while (true) {
-    const message = yield cps(getNextMessage);
-    yield put({type: eventType, worker, message});
-  }
 };
