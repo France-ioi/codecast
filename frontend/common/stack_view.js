91f6e5914be1cbbf47d6f20017f885d1a70936af
Sebastien Carlier <s.carlier@epixode.fr>
2016-04-15 12:02:01 +0000

    improve stack display

--- frontend/common/stack_view.js
+++ frontend/common/stack_view.js
@@ -5,52 +5,102 @@ import {deref} from 'persistent-c';
 
 export const StackView = EpicComponent(self => {
 
-  self.render = function () {
-    const {state} = self.props;
-    let scope = state.scope;
-    const elems = [];
+  const getFrames = function (scope) {
+    const frames = [];
+    let blocks = [];
+    let decls = [];
+    let params = [];
     while (scope) {
       switch (scope.kind) {
         case 'function':
-          elems.push(
-            <div key={scope.key}>
-              <span>{"function "}{scope.block[1].name}</span>
-            </div>
-          );
-          break;
-        case 'block':
-          elems.push(
-            <div key={scope.key}>
-              <span>{"block"}</span>
-            </div>
-          );
-          break;
-        case 'vardecl':
-          elems.push(
-            <div key={scope.key}>
-              <span>{"var "}</span>
-              <span>{scope.decl.name}</span>
-              {' = '}
-              <span>{JSON.stringify(deref(state, scope.decl.ref, scope.decl.ty))}</span>
-            </div>
-          );
+          frames.push({
+            scope: scope,
+            name: scope.block[2][0][1].identifier,
+            params: params,
+            blocks: blocks
+          });
+          params = [];
+          blocks = [];
           break;
         case 'param':
-          elems.push(
-            <div key={scope.key}>
-              <span>{"var "}</span>
-              <span>{scope.decl.name}</span>
-              {' = '}
-              <span>{JSON.stringify(deref(state, scope.decl.ref, scope.decl.ty))}</span>
-            </div>
-          );
+          params.push(scope);
+          break;
+        case 'block':
+          blocks.push({
+            scope: scope,
+            decls: decls
+          });
+          decls = [];
+          break;
+        case 'vardecl':
+          decls.push(scope)
           break;
       }
       scope = scope.parent;
     }
-    return <div className="stack-view">{elems}</div>;
+    return frames;
   };
 
+  const renderType = function (type, prec) {
+    switch (type[0]) {
+      case 'builtin':
+        return type[1];
+      case 'pointer':
+        return renderType(type[1], 1) + '*';
+    }
+    return JSON.stringify(type);
+  };
+
+  const renderValue = function (value) {
+    if (value === undefined)
+      return 'undefined';
+    switch (value[0]) {
+      case 'integer':
+        return value[1].toString();
+      case 'floating':
+        return value[1].toString();
+    }
+    return JSON.stringify(value);
+  };
+
+  const renderDecl = function (state, decl) {
+    return (
+      <div className="scope-decl">
+        <span>{renderType(decl.type, 0)}</span>
+        {' '}
+        <span>{decl.name}</span>
+        {' = '}
+        <span>{renderValue(deref(state, decl.ref, decl.type))}</span>
+      </div>
+    );
+  };
+
+  const renderFrame = function (state, frame) {
+    const key = frame.scope.key;
+    return (
+      <div key={key} className="scope-function">
+        <span>{"function "}{frame.name}</span>
+        <ul>{frame.params.map(scope =>
+          <li key={scope.key}>{renderDecl(state, scope.decl)}</li>)}</ul>
+        <div className="scope-function-blocks">
+          {frame.blocks.map(block =>
+            <div key={block.scope.key}>
+              <ul>{block.decls.map(scope =>
+                <li key={scope.key}>{renderDecl(state, scope.decl)}</li>)}</ul>
+            </div>)}
+        </div>
+      </div>
+    );
+  };
+
+  self.render = function () {
+    /* TODO: take effects since previous step as a prop */
+    const {state} = self.props;
+    const frames = getFrames(state.scope);
+    console.log('frames', frames);
+    return <div className="stack-view">{frames.map(frame =>
+      renderFrame(state, frame))}</div>;
+  };
 
 });
 
