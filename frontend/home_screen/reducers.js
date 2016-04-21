4687958f5ca3e6d5baca3fe9518057f940e9e304
Sebastien Carlier <s.carlier@epixode.fr>
2016-04-21 11:38:21 +0000

    add directive example to test program

--- frontend/home_screen/reducers.js
+++ frontend/home_screen/reducers.js
@@ -3,7 +3,7 @@ import Immutable from 'immutable';
 
 import Document from '../common/document';
 
-const initialDocument = Document.fromString("int main (int argc, char** argv) {\n    int b = 1;\n    for (int a = 1; a < 1000000; a += 1) {\n        b = b * a;\n        printf(\"%d\\n\", b);\n    }\n    return 1;\n}\n");
+const initialDocument = Document.fromString("int main (int argc, char** argv) {\n    //! showVar(b)\n    int b = 1;\n    for (int a = 1; a < 1000000; a += 1) {\n        b = b * a;\n        printf(\"%d\\n\", b);\n    }\n    return 1;\n}\n");
 const initialSelection = {start: {row: 2, column: 24}, end: {row: 2, column: 31}};
 const initialSource = Immutable.Map({document: initialDocument, selection: initialSelection});
 
