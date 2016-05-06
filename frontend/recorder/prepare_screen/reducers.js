12a648de93fbf17d42056ad05a429fc0348e2e5e
Sebastien Carlier <s.carlier@epixode.fr>
2016-05-06 07:42:12 +0000

    add fibonacci example

--- frontend/recorder/prepare_screen/reducers.js
+++ frontend/recorder/prepare_screen/reducers.js
@@ -112,6 +112,24 @@ const examples = [
       "    return 0;",
       "}"
     ].join('\n')
+  },
+  {
+    title: "fibonacci",
+    source: [
+      "int fibo(int n) {",
+      "   if (n == 0)",
+      "       return 0;",
+      "   if (n == 1)",
+      "       return 1;",
+      "   int a = fibo(n - 1);",
+      "   int b = fibo(n - 2);",
+      "   return a + b;",
+      "}",
+      "int main() {",
+      "     int n = 15;",
+      "     printf(\"fibo(%d) = %d\\n\", n, fibo(n));",
+      "}"
+    ].join('\n')
   }
   // TODO: test these expressions: a?b:c; a[b]; (int)a; c[0];
 ];
