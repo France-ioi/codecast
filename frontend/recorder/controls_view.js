3f4f4d9e680a34031bf3fa4e355011083aa2f820
Sebastien Carlier <s.carlier@epixode.fr>
2016-05-11 22:22:46 +0000

    clean up recorder controls

--- frontend/recorder/controls_view.js
+++ frontend/recorder/controls_view.js
@@ -10,29 +10,20 @@ export default function* (deps) {
   yield use(
     'recorderStop',
     'stepperStep', 'stepperInterrupt', 'stepperRestart', 'stepperExit',
-    'translate'
+    'translate', 'StepperControls', 'FullscreenButton'
   );
 
-  yield defineSelector('ControlsSelector', function (state, props) {
+  yield defineSelector('RecorderControlsSelector', function (state, props) {
     const recorder = state.get('recorder');
     const recorderState = recorder.get('state');
     const isRecording = recorderState === 'recording';
     const elapsed = Math.round(recorder.get('elapsed') / 1000) || 0;
     const eventCount = recorder.get('events').count();
-    const stepper = deps.getStepperState(state);
-    const haveStepper = !!stepper;
-    const stepperState = haveStepper && stepper.get('state');
-    const isStepping = stepperState !== 'idle';
-    const stepperDisplay = haveStepper && stepper.get('display');
-    const {control} = stepperDisplay || {};
-    const canStep = !!(!isStepping && control && control.node);
-    return {isRecording, elapsed, eventCount, haveStepper, isStepping, canStep};
+    return {isRecording, elapsed, eventCount};
   });
 
 
-  yield defineView('Controls', 'ControlsSelector', EpicComponent(self => {
-
-    // Recorder
+  yield defineView('RecorderControls', 'RecorderControlsSelector', EpicComponent(self => {
 
     const onPauseRecording = function () {
       // TODO
@@ -57,87 +48,23 @@ export default function* (deps) {
       self.props.dispatch({type: deps.playerPause});
     };
 
-    // All
-
-    const onStepExpr = function () {
-      self.props.dispatch({type: deps.stepperStep, mode: 'expr'});
-    };
-
-    const onStepInto = function () {
-      self.props.dispatch({type: deps.stepperStep, mode: 'into'});
-    };
-
-    const onStepOut = function () {
-      self.props.dispatch({type: deps.stepperStep, mode: 'out'});
-    };
-
-    const onStepOver = function () {
-      self.props.dispatch({type: deps.stepperStep, mode: 'over'});
-    };
-
-    const onInterrupt = function () {
-      self.props.dispatch({type: deps.stepperInterrupt});
-    };
-
-    const onRestart = function () {
-      self.props.dispatch({type: deps.stepperRestart});
-    };
-
-    const onEdit = function () {
-      self.props.dispatch({type: deps.stepperExit});
-    };
-
-    const onTranslate = function () {
-      self.props.dispatch({type: deps.translate});
-    };
-
-    const onEnterFullScreen = function () {
-      // TODO: replace with action that runs a saga
-      var elem = document.documentElement;
-      if (elem.requestFullscreen) {
-        elem.requestFullscreen();
-      } else if (elem.msRequestFullscreen) {
-        elem.msRequestFullscreen();
-      } else if (elem.mozRequestFullScreen) {
-        elem.mozRequestFullScreen();
-      } else if (elem.webkitRequestFullscreen) {
-        elem.webkitRequestFullscreen();
-      }
-    };
-
     self.render = function () {
       // recorder
       const {isRecording, elapsed, eventCount} = self.props;
-      // player
-      const {playerState, lastError, current} = self.props;
       return (
         <div className="pane pane-controls">
-          <p>
-
-              {/preparing|starting|ready|paused/.test(playerState) &&
-                <Button onClick={onStartPlayback} enabled={/ready|paused/.test(playerState)}>
-                  <i className="fa fa-play"/>
-                </Button>}
-              {/playing|pausing/.test(playerState) &&
-                <Button onClick={onPausePlayback} enabled={playerState === 'playing'}>
-                  <i className="fa fa-pause"/>
-                </Button>}
-              <Button onClick={onEnterFullScreen}>mode plein écran</Button>
-              <span>{playerState}</span>
-              {lastError && <p>{JSON.stringify(lastError)}</p>}
-
-            {false && <Button onClick={onPauseRecording} disabled={!isRecording}>
-              <i className="fa fa-pause"/>
-            </Button>}
-            <Button onClick={onStopRecording} disabled={!isRecording}>
-              <i className="fa fa-stop"/>
-            </Button>
-            <view.StepperControls/>
-            {' '}
-            <span><i className="fa fa-clock-o"/> {elapsed}s</span>
-            {' '}
-            <span><i className="fa fa-bolt"/> {eventCount}</span>
-          </p>
+          <Button onClick={onStopRecording} disabled={!isRecording}>
+            <i className="fa fa-stop"/>
+          </Button>
+          {false && <Button onClick={onPauseRecording} disabled={!isRecording}>
+            <i className="fa fa-pause"/>
+          </Button>}
+          <deps.FullscreenButton/>
+          {' '}
+          <span><i className="fa fa-clock-o"/> {elapsed}s</span>
+          {' '}
+          <span><i className="fa fa-bolt"/> {eventCount}</span>
+          <deps.StepperControls/>
         </div>
       );
     };
