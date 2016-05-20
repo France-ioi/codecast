82ec88b548c05c854967eb99d20ab8a0ebfb28a9
Sebastien Carlier <s.carlier@epixode.fr>
2016-05-20 06:17:39 +0000

    examples menu: pull right

--- frontend/common/examples.js
+++ frontend/common/examples.js
@@ -211,7 +211,7 @@ export default function* (deps) {
       const {examples, disabled} = self.props;
       return (
         <Nav bsStyle="pills" className="nav-examples">
-          <NavDropdown title="Exemples" disabled={disabled}>
+          <NavDropdown title="Exemples" disabled={disabled} pullRight={true}>
             {examples.map((example, i) => <MenuItem key={i} eventKey={i} onSelect={onSelectExample}>{example.title}</MenuItem>)}
           </NavDropdown>
         </Nav>
