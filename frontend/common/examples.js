b378db2757435f267922f1c13d879bc38690d850
Sebastien Carlier <s.carlier@epixode.fr>
2016-06-15 18:06:57 +0000

    add compound type example

--- frontend/common/examples.js
+++ frontend/common/examples.js
@@ -152,6 +152,30 @@ const examples = [
       "    return 0;",
       "}"
     ].join('\n')
+  },
+
+  {
+    title: "types composés",
+    source: [
+      "#include <stdio.h>",
+      "int main() {",
+      "",
+      "    // array of pointer to int",
+      "    int *a[1];",
+      "    int a_value = 1;",
+      "    a[0] = &a_value;",
+      "",
+      "    // declare b as pointer to array of int",
+      "    int (*b)[];",
+      "    int b_value[1];",
+      "    b = &b_value;",
+      "",
+      "    // declare foo as array 3 of array 2 of pointer to pointer to function returning pointer to array of pointer to char",
+      "    char *(*(**foo[3][2])())[];",
+      "",
+      "    return 0;",
+      "}"
+    ].join('\n')
   }
 
   // TODO: add example for a?b:c
