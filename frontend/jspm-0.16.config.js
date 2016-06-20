b1ea566d1e6cc0fede46106c98c8af8a0f5ecd69
Sebastien Carlier <s.carlier@epixode.fr>
2016-06-20 09:00:12 +0000

    use persistent-c 5.7.2 (LNot fix)

--- frontend/jspm-0.16.config.js
+++ frontend/jspm-0.16.config.js
@@ -57,7 +57,7 @@ System.config({
     "immutable": "npm:immutable@3.8.1",
     "lamejs": "npm:lamejs@1.1.0",
     "lodash": "npm:lodash@4.12.0",
-    "persistent-c": "npm:persistent-c@5.7.0",
+    "persistent-c": "npm:persistent-c@5.7.2",
     "query-string": "npm:query-string@4.1.0",
     "rc-slider": "npm:rc-slider@3.6.0",
     "react": "npm:react@15.0.1",
@@ -703,7 +703,7 @@ System.config({
       "process": "github:jspm/nodelibs-process@0.1.2",
       "systemjs-json": "github:systemjs/plugin-json@0.1.2"
     },
-    "npm:persistent-c@5.7.0": {
+    "npm:persistent-c@5.7.2": {
       "child_process": "github:jspm/nodelibs-child_process@0.1.0",
       "immutable": "npm:immutable@3.8.1",
       "text-encoding-utf-8": "npm:text-encoding-utf-8@1.0.1"
