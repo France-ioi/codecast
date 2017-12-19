9d83104775ee3e468b91b8544632dbe606971bcd
Sebastien Carlier <s.carlier@epixode.fr>
2017-12-19 09:46:38 +0000

    fix vumeter

--- frontend/recorder/vumeter.js
+++ frontend/recorder/vumeter.js
@@ -4,14 +4,29 @@ import {takeEvery, call, fork, cancel, select} from 'redux-saga/effects';
 import {delay} from 'redux-saga';
 
 class Vumeter extends React.PureComponent {
-  render() {
-    return <canvas id="vumeter" width="10" height="100"></canvas>;
+  render () {
+    return <canvas ref={this._refCanvas} id='vumeter' width="10" height="100"></canvas>;
   }
+  componentDidMount () {
+    this.props.dispatch({type: this.props.vumeterMounted, payload: {element: this._canvas}});
+  }
+  _refCanvas = (el) => {
+    this._canvas = el;
+  };
+}
+
+function VumeterSelector (state) {
+  const {vumeterMounted} = state.get('scope');
+  return {vumeterMounted};
 }
 
 function* vumeterSaga () {
-  const {recorderStopped, recorderPreparing} = yield select(state => state.get('scope'));
+  const {recorderStopped, recorderPreparing, vumeterMounted} = yield select(state => state.get('scope'));
   let vumeterTask;
+  let canvasContext;
+  yield takeEvery(vumeterMounted, function* ({payload: {element}}) {
+    canvasContext = element && element.getContext("2d");
+  });
   yield takeEvery(recorderStopped, vumeterCleanupSaga);
   yield takeEvery(recorderPreparing, function* ({payload: {analyser}}) {
     if (!analyser) return;
@@ -19,10 +34,8 @@ function* vumeterSaga () {
     vumeterTask = yield fork(vumeterMonitorSaga, analyser);
   });
   function* vumeterMonitorSaga (analyser) {
-    // Set up the ScriptProcessor to divert all buffers to the worker.
-    const vumeterElement = document.getElementById('vumeter');
-    const canvasContext = vumeterElement.getContext("2d");
     const vumeterData = new Uint8Array(analyser.frequencyBinCount);
+    // Set up the ScriptProcessor to divert all buffers to the worker.
     periodically(function* () {
       // Get analyser data and update vumeter.
       analyser.getByteFrequencyData(vumeterData);
@@ -45,15 +58,23 @@ function* vumeterSaga () {
   }
   function* periodically (saga) {
     while (true) {
-      yield call(saga);
+      if (canvasContext) {
+        yield call(saga);
+      }
       yield call(delay, 100);
     }
   }
 }
 
+function vumeterMountedReducer (state, {payload: {element}}) {
+  return state.set('vumeterElement', element);
+}
+
 export default function (bundle, deps) {
 
+  bundle.defineAction('vumeterMounted', 'Vumeter.Mounted');
+  bundle.addReducer('vumeterMounted', vumeterMountedReducer);
   bundle.addSaga(vumeterSaga);
-  bundle.defineView('Vumeter', Vumeter);
+  bundle.defineView('Vumeter', VumeterSelector, Vumeter);
 
 };
