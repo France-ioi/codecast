e37b1e22317fe40e7f8b445a1483f42b7110e6a3
Melanie Storup <m.storup@epixode.fr>
2018-01-15 14:29:50 +0100

    recorder global controls (logout)

--- frontend/recorder/app_view.js
+++ frontend/recorder/app_view.js
@@ -1,14 +1,15 @@
 
 import React from 'react';
+import classnames from 'classnames';
 
 class RecorderApp extends React.PureComponent {
   render () {
-    const {LogoutButton, Screen, ErrorView, MemoryUsage, Vumeter} = this.props;
+    const {RecorderGlobalControls, Screen, ErrorView, MemoryUsage, Vumeter} = this.props;
     return (
       <div className='container'>
+        <RecorderGlobalControls/>
         <div id='page-level-controls'>
           <div>
-            <LogoutButton/>
             <MemoryUsage/>
             <Vumeter/>
           </div>
@@ -22,7 +23,7 @@ class RecorderApp extends React.PureComponent {
 
 function RecorderAppSelector (state, props) {
   const scope = state.get('scope');
-  const {LogoutButton, ErrorView, MemoryUsage, Vumeter} = scope;
+  const {ErrorView, MemoryUsage, Vumeter, RecorderGlobalControls} = scope;
   const user = state.get('user');
   const screen = state.get('screen');
   let Screen;
@@ -33,10 +34,38 @@ function RecorderAppSelector (state, props) {
   } else if (screen === 'save') {
     Screen = scope.SaveScreen;
   }
-  return {LogoutButton, Screen, ErrorView, MemoryUsage, Vumeter};
+  return {Screen, ErrorView, MemoryUsage, Vumeter, RecorderGlobalControls};
+}
+
+function RecorderGlobalControlsSelector (state) {
+  const {LogoutButton} = state.get('scope');
+  return {LogoutButton};
+}
+
+class RecorderGlobalControls extends React.PureComponent {
+  render () {
+    const {LogoutButton} = this.props;
+    const {collapsed} = this.state;
+    return (
+      <div id='global-controls' className={classnames({collapsed})}>
+        <span className='collapse-toggle' onClick={this._toggleCollapsed}>
+          <i className={`fa fa-chevron-${collapsed ? 'down' : 'up'}`}/>
+        </span>
+        <div className='btn-group'>
+          <LogoutButton/>
+        </div>
+      </div>
+    );
+  }
+  state = {collapsed: false};
+  _toggleCollapsed = () => {
+    const {collapsed} = this.state;
+    this.setState({collapsed: !collapsed});
+  };
 }
 
 export default function (bundle, deps) {
   bundle.use('LoginScreen', 'RecordScreen', 'SaveScreen', 'ErrorView', 'MemoryUsage', 'Vumeter');
   bundle.defineView('RecorderApp', RecorderAppSelector, RecorderApp);
+  bundle.defineView('RecorderGlobalControls', RecorderGlobalControlsSelector, RecorderGlobalControls);
 };
