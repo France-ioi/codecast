37391edaf0c601339dfba30f1c3dc8fa9fc3bf14
Sebastien Carlier <s.carlier@epixode.fr>
2018-05-18 14:29:42 +0000

    examples: clear source (and mode) in callback url

--- frontend/common/examples.js
+++ frontend/common/examples.js
@@ -6,21 +6,33 @@ export default function (bundle, deps) {
 
   bundle.defineAction('examplesUrlChanged', 'Examples.Url.Changed');
   bundle.addReducer('examplesUrlChanged', function (state, {payload: {examplesUrl, callbackUrl}}) {
-    return state.set('examples', {examplesUrl, callbackUrl});
+    examplesUrl = makeExamplesUrl(state, callbackUrl, examplesUrl);
+    return state.set('examples', {examplesUrl});
   });
 
   function ExamplePickerSelector (state, props) {
     const getMessage = state.get('getMessage');
-    let {examplesUrl, callbackUrl} = state.get('examples', {});
-    if (examplesUrl) {
-      examplesUrl = url.parse(examplesUrl, true);
-      examplesUrl.query.target = '_self';
-      examplesUrl.query.tags = state.get('mode', 'plain');
-      examplesUrl.query.lang = state.get('language', 'en').replace(/_.*$/, '');
-      examplesUrl.query.callback = callbackUrl;
-      examplesUrl = url.format(examplesUrl);
-    }
+    let {examplesUrl} = state.get('examples', {});
     return {examplesUrl, getMessage};
+  }
+
+  function makeExamplesUrl (state, callbackUrl, examplesUrl) {
+    if (!examplesUrl) return false;
+    /* Clean up the callback URL to avoid passing the current source to the
+       examples selector.  Also clear 'mode' in case the user changes it in
+       the selector. */
+    callbackUrl = url.parse(callbackUrl, true);
+    delete callbackUrl.search; // force url.format to rebuild the search string
+    delete callbackUrl.query.source;
+    delete callbackUrl.query.mode;
+    callbackUrl = url.format(callbackUrl);
+    examplesUrl = url.parse(examplesUrl, true);
+    examplesUrl.query.target = '_self';
+    examplesUrl.query.tags = state.get('mode', 'plain');
+    examplesUrl.query.lang = state.get('language', 'en').replace(/_.*$/, '');
+    examplesUrl.query.callback = callbackUrl;
+    examplesUrl = url.format(examplesUrl);
+    return examplesUrl;
   }
 
   bundle.defineView('ExamplePicker', ExamplePickerSelector, class ExamplePicker extends React.PureComponent {
