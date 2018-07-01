2e2b32b4a43e3a10e263a73b537c6f4ffaccc220
Sebastien Carlier <s.carlier@epixode.fr>
2018-07-01 12:29:01 +0000

    player: allow async calls during computation of instants

--- frontend/player/replay.js
+++ frontend/player/replay.js
@@ -7,9 +7,10 @@ export default function (bundle) {
   const replayApi = {};
   bundle.defineValue('replayApi', replayApi);
 
-  /* For each event a number of callbacks can be registered.
-     When an event is replayed, each callback is invoked in order to update
-     the context. */
+  /* For each event a number of handlers can be registered.
+     An event is replayed by calling (in registration order) the handlers
+     registered with the event, each handler being tasked with updating the
+     context. */
   const eventHandlers = new Map();
   replayApi.on = function (keys, saga) {
     if (typeof keys === 'string') {
@@ -26,11 +27,11 @@ export default function (bundle) {
       sagas.push(saga);
     }
   };
-  replayApi.applyEvent = function (key, context, event, instant) {
+  replayApi.applyEvent = function* (key, context, event, instant) {
     if (eventHandlers.has(key)) {
       const funcs = eventHandlers.get(key, []);
       for (var func of funcs) {
-        func(context, event, instant);
+        yield call(func, context, event, instant);
       }
     } else {
       console.log(`event ${key} ignored (no replay handler)`);
