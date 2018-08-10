61cd25783d888b9724392631ca8f317c1da709fc
Sebastien Carlier <s.carlier@epixode.fr>
2018-08-10 18:11:10 +0000

    sort view fixes

--- frontend/stepper/views/sort.js
+++ frontend/stepper/views/sort.js
@@ -6,6 +6,7 @@ import range from 'node-range';
 
 import {getNumber, getList, viewExprs, evalExpr, renderValue, renderArrow} from './utils';
 import {extractView} from './array_utils';
+import {SvgPan} from './svg-pan';
 
 const marginLeft = 100;
 const marginTop = 4;
@@ -29,8 +30,8 @@ function getValueClass (content) {
     'default');
 }
 
-function Bar ({cell}) {
-  const {position, index, address, content, gap} = cell;
+function Bar ({view, bar}) {
+  const {position, index, address, content, gap} = bar;
   const w1 = barWidth + barSpacing;        // w1: total bar width
   const y1 = barHeight;                    // y1: relative bottom corner of bar rect
   const y5 = y1 + textLineHeight - textBaseline + barMarginBottom; // y5: baseline of the index label
@@ -49,7 +50,7 @@ function Bar ({cell}) {
   const rectClass = getValueClass(content);
   const y3 = y1 - textBaseline - barPaddingBottom;
   const y4 = y3 - textLineHeight;
-  const h1 = barHeight * content.current.toInteger() / this.maxValue; // y3: bar height based on value
+  const h1 = barHeight * content.current.toInteger() / view.maxValue; // y3: bar height based on value
   const h2 = (textLineHeight - textBaseline) / 3 // strike-through height from line
   return (
     <g className="bar" transform={`translate(${x0},${y0})`}>
@@ -86,14 +87,14 @@ function Cursor ({cursor}) {
   );
 }
 
-function Threshold (decl) {
-  const {label, error, value} = decl;
+function Threshold ({view, threshold}) {
+  const {label, error, value} = threshold;
   if (error || !('number' in value))
     return false;
   const x0 = marginLeft - thresholdLineExt;
-  const x1 = marginLeft + (barWidth + barSpacing) * this.nbCells + thresholdLineExt;
+  const x1 = marginLeft + (barWidth + barSpacing) * view.nbCells + thresholdLineExt;
   const x2 = marginLeft - thresholdMarginRight;
-  const y0 = marginTop + barHeight * value.number / this.maxValue;
+  const y0 = marginTop + barHeight * value.number / view.maxValue;
   const y1 = y0 + textBaseline;
   return (
     <g className="threshold">
@@ -104,19 +105,6 @@ function Threshold (decl) {
 }
 
 export class SortView extends React.PureComponent {
-
-  onViewChange = (event) => {
-    const {value} = event;
-    // Prevent vertical panning.
-    value.matrix.f = 0;
-    const update = {viewState: value};
-    this.props.onChange(this.props.directive, update);
-  };
-
-  getViewState = (controls) => {
-    const viewState = controls.get('viewState');
-    return viewState || ViewerHelper.getDefaultValue();
-  };
 
   render () {
     const {Frame, controls, directive, frames, context, scale, getMessage} = this.props;
@@ -130,11 +118,13 @@ export class SortView extends React.PureComponent {
     const expr = byPos[0];
     const {dim} = byName;
     const getOptions = {core: core, frame: topFrame};
+    const cellPan = this.getPosition();
     const thExprs = getList(byName.thresholds, []);
     const cursorExprs = getList(byName.cursors, []);
     const cursorRows = getNumber(byName.cursorRows, 1);
     const maxVisibleCells = getNumber(byName.n, 40);
     const height = getNumber(byName.height, 'auto');
+    const svgHeight = marginTop + barHeight + barMarginBottom + textLineHeight + minArrowHeight + textLineHeight * cursorRows + marginBottom;
     const view = {dimExpr: dim, fullView, cursorExprs, maxVisibleCells, cursorRows, getMessage};
     Object.assign(view, extractView(context, topFrame, expr, view));
     if (view.error) {
@@ -142,10 +132,6 @@ export class SortView extends React.PureComponent {
     }
     view.thresholds = viewExprs(core, topFrame, thExprs);
     view.nbCells = view.cells.length;
-    const viewState = getViewState(controls);
-    const svgWidth = marginLeft + (barWidth + barSpacing) * view.nbCells;
-    const svgHeight = marginTop + barHeight + barMarginBottom + textLineHeight + minArrowHeight + textLineHeight * cursorRows + marginBottom;
-    const divHeight = ((height === 'auto' ? svgHeight : height) * scale) + 'px';
     // Find the largest cell value.
     let maxValue = 0;
     view.cells.forEach(function (cell) {
@@ -162,29 +148,34 @@ export class SortView extends React.PureComponent {
     return (
       <Frame {...this.props} hasFullView>
         <div className='clearfix' style={{padding: '2px'}}>
-          <div style={{width: '100%', height: divHeight}}>
-            <ViewerResponsive tool='pan' value={viewState} onChange={onViewChange} background='transparent' specialKeys={[]}>
-              <svg width={svgWidth} height={svgHeight} version="1.1" xmlns="http://www.w3.org/2000/svg">
-                <clipPath id="barClipping">
-                  <rect x="0" y="0" width={barWidth} height={barHeight}/>
-                </clipPath>
-                <g className="sort-view">
-                  <g className="cursors">
-                    {view.cursorMap.map(cursor => <Cursor key={cursor.index} view={view} cursor={cursor} />)}
-                  </g>
-                  <g className="cells">
-                    {view.cells.map(bar => <Bar key={bar.index} view={view} bar={bar} />)}
-                  </g>
-                  <g className="thresholds">
-                    {view.thresholds.map((threshold, i) => <Threshold key={i} view={view} threshold={threshold} />)}
-                  </g>
-                </g>
-              </svg>
-            </ViewerResponsive>
-          </div>
+          <SvgPan width='100%' height={svgHeight} scale={scale} x={cellPan * (barWidth + barSpacing) - 10} y={0} getPosition={this.getPosition} onPan={this.onPan} >
+            <clipPath id="barClipping">
+              <rect x="0" y="0" width={barWidth} height={barHeight}/>
+            </clipPath>
+            <g className="sort-view">
+              <g className="cursors">
+                {view.cursorMap.map(cursor => <Cursor key={cursor.index} view={view} cursor={cursor} />)}
+              </g>
+              <g className="cells">
+                {view.cells.map(bar => <Bar key={bar.index} view={view} bar={bar} />)}
+              </g>
+              <g className="thresholds">
+                {view.thresholds.map((threshold, i) => <Threshold key={i} view={view} threshold={threshold} />)}
+              </g>
+            </g>
+          </SvgPan>
         </div>
       </Frame>
     );
   }
+
+  getPosition = () => {
+    return this.props.controls.get('cellPan', 0);
+  };
+
+  onPan = (startPosition, dx, dy) => {
+    const cellPan = startPosition - (dx / (barWidth + barSpacing));
+    this.props.onChange(this.props.directive, {cellPan});
+  };
 
 }
