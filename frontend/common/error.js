ccdd6ca2b2c63faab8c320cb1cce1b97ab7f4841
Sebastien Carlier <s.carlier@epixode.fr>
2018-09-14 13:45:52 +0000

    fix clearing the error

--- frontend/common/error.js
+++ frontend/common/error.js
@@ -4,7 +4,9 @@ import {Dialog} from '@blueprintjs/core';
 
 export default function (bundle) {
   bundle.defineAction('error', 'Error');
+  bundle.defineAction('clearError', 'Error.Clear');
   bundle.addReducer('error', errorReducer);
+  bundle.addReducer('clearError', clearErrorReducer);
   bundle.defineView('AppErrorBoundary', AppErrorBoundarySelector, AppErrorBoundary);
 }
 
@@ -15,10 +17,6 @@ function AppErrorBoundarySelector (state) {
 }
 
 class AppErrorBoundary extends React.Component {
-  componentDidCatch (error, info) {
-    const {dispatch, actionTypes} = this.props;
-    dispatch({type: actionTypes.error, payload: {source: 'react', error, info}});
-  }
   render() {
     const {lastError, children} = this.props;
     if (!lastError) {
@@ -45,11 +43,21 @@ class AppErrorBoundary extends React.Component {
       </div>
     );
   }
+  componentDidCatch (error, info) {
+    const {dispatch, actionTypes} = this.props;
+    dispatch({type: actionTypes.error, payload: {source: 'react', error, info}});
+  }
   _clearError = () => {
+    const {dispatch, actionTypes} = this.props;
+    this.props.dispatch({type: actionTypes.clearError});
   };
 }
 
 function errorReducer (state, {payload}) {
   console.log("GENERIC ERROR", payload);
   return state.set('lastError', payload);
+}
+
+function clearErrorReducer (state, _action) {
+  return state.set('lastError', undefined);
 }
