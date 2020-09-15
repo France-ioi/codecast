3b62346026ccce0bb13e1a6f18b09f25fce9efaf
Geoffrey Huck <geoffrey@geot.fr>
2020-09-15 16:27:39 +0200

    Toggle object view

--- frontend/stepper/python/analysis/components/PythonVariable.js
+++ frontend/stepper/python/analysis/components/PythonVariable.js
@@ -1,76 +1,27 @@
 import * as React from 'react';
 import PythonVariableValue from "./PythonVariableValue";
 
-class PythonVariable extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            opened: true
-        };
-    }
-
-    toggleOpened = () => {
-        this.setState((state) => ({
-            opened: !state.opened
-        }));
-    }
-
-    render() {
-        let classes = 'variable-container';
-        if (this.props.value.cur instanceof Sk.builtin.object) {
-            if (this.props.value.cur.hasOwnProperty('$d') || this.props.value.cur instanceof Sk.builtin.dict) {
-                classes += ' vardecl-object';
-
-                if (this.state.opened) {
-                    return (
-                        <div className={classes}>
-                            <div className="object-toggle" onClick={this.toggleOpened}>
-                                <span className="toggle-icon">▾</span>
-                                <span className="variable-name">{this.props.name}</span>
-                                {' = '}
-                            </div>
-                            <span className="vardecl-value">
-                                <span className="value">
-                                    <PythonVariableValue cur={this.props.value.cur} old={this.props.value.old} visited={this.props.visited} opened={this.state.opened} />
-                                </span>
-                            </span>
-                        </div>
-                    );
-                }
-
-                return (
-                    <div className={classes}>
-                        <div className="object-toggle" onClick={this.toggleOpened}>
-                            <span className="toggle-icon">▸</span>
-                            <span className="variable-name">{this.props.name}</span>
-                            {' = '}
-                            <span className="vardecl-value">
-                                <span className="value">
-                                    <PythonVariableValue cur={this.props.value.cur} old={this.props.value.old} visited={this.props.visited} opened={this.state.opened} />
-                                </span>
-                            </span>
-                        </div>
-                    </div>
-                );
-            }
+const PythonVariable = (props) => {
+    let classes = 'variable-container';
+    if (props.value.cur instanceof Sk.builtin.object) {
+        if (props.value.cur.hasOwnProperty('$d') || props.value.cur instanceof Sk.builtin.dict) {
+            classes += ' vardecl-object';
         }
-
-        return (
-            <React.Fragment>
-                <span style={{color: 'transparent'}}>▸</span>
-                <span>
-                    <span className="variable-name">{this.props.name}</span>
-                </span>
-                {' = '}
-                <span className="vardecl-value">
-                    <span className="value">
-                        <PythonVariableValue cur={this.props.value.cur} old={this.props.value.old} visited={this.props.visited} opened={this.state.opened} />
-                    </span>
-                </span>
-            </React.Fragment>
-        )
     }
+
+    return (
+        <span className={classes}>
+            <span>
+                <span className="variable-name">{props.name}</span>
+            </span>
+            {' = '}
+            <span className="vardecl-value">
+                <span className="value">
+                    <PythonVariableValue cur={props.value.cur} old={props.value.old} visited={props.visited} />
+                </span>
+            </span>
+        </span>
+    )
 }
 
 export default PythonVariable;
--- frontend/stepper/python/analysis/components/PythonVariableValue.js
+++ frontend/stepper/python/analysis/components/PythonVariableValue.js
@@ -1,180 +1,207 @@
 import * as React from 'react';
 import PythonVariable from "./PythonVariable";
 
-const PythonVariableValue = (props) => {
-    if (props.cur instanceof Sk.builtin.dict) {
-        /**
-         * A dict's representation is as follow :
-         *
-         * test : Sk.builtin.dict
-         *   - buckets
-         *     - 14: (hash index example) /!\ This one only in case of an Object /!\
-         *       - $hash : Sk.builtin.int_
-         *         - v: 14 (hash index example)
-         *       - items : [
-         *         - 0 :
-         *           - lhs : Sk.builtin.str
-         *             - v : "__dict__"
-         *             - $savedHash : (= $hash)
-         *           - rhs : dict (= $d)
-         *     - 16: (hash index example 2)
-         *       - $hash : Sk.builtin.int_
-         *         - v: 16 (hash index example 2)
-         *       - items : [
-         *         - 0 :
-         *           - lhs : Sk.builtin.int_
-         *             - v : "a" (variable name)
-         *             - $savedHash : (= $hash)
-         *           - rhs : Sk.builtin.int_
-         *             - v: 41 (variable value)
-         *       ]
-         */
+class PythonVariableValue extends React.Component {
+    constructor(props) {
+        super(props);
 
-        const elements = [];
-        let isEmpty = true;
-        for (let hashKey in props.cur.buckets) {
-            const element = props.cur.buckets[hashKey].items[0];
-
-            // Ignore the element with name __dict__ that appears in objects dictionnaries.
-            if (element.lhs.v === '__dict__') {
-                continue;
-            }
-
-            let old = undefined;
-            if (props.old && props.old instanceof Sk.builtin.dict) {
-                const oldBucket = props.old.buckets[hashKey];
-                if (oldBucket) {
-                    // if (oldBucket.items[0].lhs.v === element.lhs.v) {
-                    //    TODO: Is this check usefull ?
-                    // }
-
-                    old = oldBucket.items[0].rhs;
-                }
-            }
-
-            elements.push({
-                name: element.lhs.v,
-                value: {
-                    cur: element.rhs,
-                    old: old
-                }
-            });
-            isEmpty = false;
-        }
-
-        const wasVisited = props.visited[props.cur._uuid];
-        const visited = {
-            ...props.visited,
-
-        }
-        visited[props.cur._uuid] = true;
-
-        return (
-            <React.Fragment>
-                {props.opened ? (
-                    <ul className="object_scope">
-                        {wasVisited ? '...' : (
-                            (isEmpty) ? (
-                                <span className="value-empty">&lt;&gt;</span>
-                            ) : (
-                                elements.map((element) => (
-                                    <li key={element.name}>
-                                        <PythonVariable name={element.name} value={element.value} visited={visited}/>
-                                    </li>
-                                ))
-                            )
-                        )}
-                    </ul>
-                ) : (
-                    <span className="value-object-closed">
-                        &lt;obj&gt;
-                    </span>
-                )}
-            </React.Fragment>
-        )
+        this.state = {
+            opened: false
+        };
     }
 
-    if (props.cur instanceof Sk.builtin.list || props.cur instanceof Sk.builtin.tuple) {
-        const nbElements = props.cur.v.length;
+    toggleOpened = () => {
+        this.setState((state) => ({
+            opened: !state.opened
+        }));
+    }
 
-        const elements = [];
-        for (let idx = 0; idx < props.cur.v.length; idx++) {
-            let old = undefined;
-            if (props.old && props.old instanceof Sk.builtin.list) {
-                old = props.old.v[idx];
+    render() {
+        if (this.props.cur instanceof Sk.builtin.dict) {
+            /**
+             * A dict's representation is as follow :
+             *
+             * test : Sk.builtin.dict
+             *   - buckets
+             *     - 14: (hash index example) /!\ This one only in case of an Object /!\
+             *       - $hash : Sk.builtin.int_
+             *         - v: 14 (hash index example)
+             *       - items : [
+             *         - 0 :
+             *           - lhs : Sk.builtin.str
+             *             - v : "__dict__"
+             *             - $savedHash : (= $hash)
+             *           - rhs : dict (= $d)
+             *     - 16: (hash index example 2)
+             *       - $hash : Sk.builtin.int_
+             *         - v: 16 (hash index example 2)
+             *       - items : [
+             *         - 0 :
+             *           - lhs : Sk.builtin.int_
+             *             - v : "a" (variable name)
+             *             - $savedHash : (= $hash)
+             *           - rhs : Sk.builtin.int_
+             *             - v: 41 (variable value)
+             *       ]
+             */
+
+            const elements = [];
+            let isEmpty = true;
+            for (let hashKey in this.props.cur.buckets) {
+                const element = this.props.cur.buckets[hashKey].items[0];
+
+                // Ignore the element with name __dict__ that appears in objects dictionnaries.
+                if (element.lhs.v === '__dict__') {
+                    continue;
+                }
+
+                let old = undefined;
+                if (this.props.old && this.props.old instanceof Sk.builtin.dict) {
+                    const oldBucket = this.props.old.buckets[hashKey];
+                    if (oldBucket) {
+                        // if (oldBucket.items[0].lhs.v === element.lhs.v) {
+                        //    TODO: Is this check usefull ?
+                        // }
+
+                        old = oldBucket.items[0].rhs;
+                    }
+                }
+
+                elements.push({
+                    name: element.lhs.v,
+                    value: {
+                        cur: element.rhs,
+                        old: old
+                    }
+                });
+                isEmpty = false;
             }
 
-            elements.push({
-                cur: props.cur.v[idx],
-                old: old
-            });
+            const wasVisited = this.props.visited[this.props.cur._uuid];
+            const visited = {
+                ...this.props.visited,
+
+            }
+            visited[this.props.cur._uuid] = true;
+
+            return (
+                <React.Fragment>
+                    {this.state.opened ? (
+                        <React.Fragment>
+                            <span className="object-toggle object-toggle-open" onClick={this.toggleOpened}>
+                                <span className="toggle-icon">▾</span>
+                            </span>
+                            <ul className="object_scope">
+                                {wasVisited ? '...' : (
+                                    (isEmpty) ? (
+                                        <span className="value-empty">&lt;&gt;</span>
+                                    ) : (
+                                        elements.map((element) => (
+                                            <li key={element.name}>
+                                                <PythonVariable name={element.name} value={element.value} visited={visited}/>
+                                            </li>
+                                        ))
+                                    )
+                                )}
+                            </ul>
+                        </React.Fragment>
+                    ) : (
+                        <span className="object-toggle" onClick={this.toggleOpened}>
+                            <span className="toggle-icon">▸</span>
+                            <span className="value-object-closed">
+                                &lt;obj&gt;
+                            </span>
+                        </span>
+                    )}
+                </React.Fragment>
+            )
         }
 
-        const wasVisited = props.visited[props.cur._uuid];
-        const visited = {
-            ...props.visited,
+        if (this.props.cur instanceof Sk.builtin.list || this.props.cur instanceof Sk.builtin.tuple) {
+            const nbElements = this.props.cur.v.length;
 
-        }
-        visited[props.cur._uuid] = true;
+            const elements = [];
+            for (let idx = 0; idx < this.props.cur.v.length; idx++) {
+                let old = undefined;
+                if (this.props.old && this.props.old instanceof Sk.builtin.list) {
+                    old = this.props.old.v[idx];
+                }
 
-        return (
-            <React.Fragment>
-                [{wasVisited ? '...' : (
+                elements.push({
+                    cur: this.props.cur.v[idx],
+                    old: old
+                });
+            }
+
+            const wasVisited = this.props.visited[this.props.cur._uuid];
+            const visited = {
+                ...this.props.visited,
+
+            }
+            visited[this.props.cur._uuid] = true;
+
+            return (
+                <React.Fragment>
+                    [{wasVisited ? '...' : (
                     elements.map((element, index) => (
                         <span key={index}>
-                            <PythonVariableValue cur={element.cur} old={element.old} visited={visited} />
+                                <PythonVariableValue cur={element.cur} old={element.old} visited={visited}/>
                             {(index + 1) < nbElements ? ', ' : null}
-                        </span>
+                            </span>
                     ))
                 )}]
-            </React.Fragment>
-        )
-    }
-
-    if (props.cur instanceof Sk.builtin.str) {
-        return (
-            <React.Fragment>
-                <span className="value-string">"{props.cur.v}"</span>
-                {(props.old && (props.cur.v !== props.old.v)) ? <span className="value-previous">"{props.old.v}"</span> : null }
-            </React.Fragment>
-        )
-    }
-
-    if (props.cur instanceof Sk.builtin.object && props.cur.hasOwnProperty('$d')) {
-        /**
-         * An object's representation is as follow :
-         *
-         * test : Sk.builtin.object
-         *   - $d : Sk.builtin.dict
-         */
-
-        let old = props.old;
-        if (old && old instanceof Sk.builtin.object) {
-            old = old.$d;
+                </React.Fragment>
+            )
         }
 
-        const wasVisited = props.visited[props.cur._uuid];
-        const visited = {
-            ...props.visited,
-
+        if (this.props.cur instanceof Sk.builtin.str) {
+            return (
+                <React.Fragment>
+                    <span className="value-string">"{this.props.cur.v}"</span>
+                    {(this.props.old && (this.props.cur.v !== this.props.old.v)) ?
+                        <span className="value-previous">"{this.props.old.v}"</span>
+                    : null}
+                </React.Fragment>
+            )
         }
-        visited[props.cur._uuid] = true;
+
+        if (this.props.cur instanceof Sk.builtin.object && this.props.cur.hasOwnProperty('$d')) {
+            /**
+             * An object's representation is as follow :
+             *
+             * test : Sk.builtin.object
+             *   - $d : Sk.builtin.dict
+             */
+
+            let old = this.props.old;
+            if (old && old instanceof Sk.builtin.object) {
+                old = old.$d;
+            }
+
+            const wasVisited = this.props.visited[this.props.cur._uuid];
+            const visited = {
+                ...this.props.visited,
+
+            }
+            visited[this.props.cur._uuid] = true;
+
+            return (
+                <React.Fragment>
+                    {wasVisited ? '...' : (
+                        <PythonVariableValue cur={this.props.cur.$d} old={old} visited={visited} />
+                    )}
+                </React.Fragment>
+            )
+        }
 
         return (
             <React.Fragment>
-                {wasVisited ? '...' : (
-                    <PythonVariableValue cur={props.cur.$d} old={old} visited={visited} opened={props.opened} />
-                )}
+                <span className="value-scalar">{this.props.cur.v}</span>
+                {(this.props.old && (this.props.cur.v !== this.props.old.v)) ?
+                    <span className="value-previous">{this.props.old.v}</span> : null}
             </React.Fragment>
-        )
+        );
     }
-
-    return (
-        <React.Fragment>
-            <span className="value-scalar">{props.cur.v}</span>
-            {(props.old && (props.cur.v !== props.old.v)) ? <span className="value-previous">{props.old.v}</span> : null }
-        </React.Fragment>
-    );
-};
+}
 
 export default PythonVariableValue;
