66f26efcaee4dcf60736bbc65f71a6cbb5151e23
Sébastien Tainon <arkandias1@hotmail.fr>
2021-08-19 18:03:36 +0200

    Repair yarn build/lint for Git workflows

--- frontend/task/TaskInstructions.tsx
+++ frontend/task/TaskInstructions.tsx
@@ -7,13 +7,13 @@ export class TaskInstructions extends React.PureComponent {
                 <h1>Votre mission</h1>
 
                 <p>
-                    Programmez le robot pour qu'il pousse les caisses sur les cases marquées.
+                    Programmez le robot pour qu&apos;il pousse les caisses sur les cases marquées.
                 </p>
                 <p>
-                    Pour pousser une caisse, mettez d'abord le robot face à la caisse, il avancera en la poussant.
+                    Pour pousser une caisse, mettez d&apos;abord le robot face à la caisse, il avancera en la poussant.
                 </p>
                 <p>
-                    <strong>Attention :</strong> vous ne pouvez utiliser qu'une fois l'instruction "pousser la caisse".
+                    <strong>Attention :</strong> vous ne pouvez utiliser qu&apos;une fois l&apos;instruction "pousser la caisse".
                 </p>
             </div>
         );
