8304c2591611e6b0de5e064d045fc6cf49bd79db
Sébastien Tainon <arkandias1@hotmail.fr>
2023-04-19 18:26:57 +0200

    Cleanup

--- frontend/task/libs/smart_contract/smart_contract_lib.ts
+++ frontend/task/libs/smart_contract/smart_contract_lib.ts
@@ -1,15 +1,11 @@
 import {QuickAlgoLibrary} from "../quickalgo_library";
 import {call, put, takeEvery} from "typed-redux-saga";
-import {AppStore} from "../../../store";
-import {ActionTypes as StepperActionTypes, stepperDisplayError} from "../../../stepper/actionTypes";
+import {ActionTypes as StepperActionTypes} from "../../../stepper/actionTypes";
 import {App} from "../../../index";
-import {IoMode} from "../../../stepper/io";
 import log from 'loglevel';
 import {SmartContractView} from './SmartContractView';
 import {
     TaskSubmissionServerTestResult,
-    TaskSubmissionTestResult,
-    TestResultDiffLog
 } from '../../../submission/submission';
 import {appSelect} from '../../../hooks';
 import {
@@ -17,105 +13,6 @@ import {
     QuickAlgoLibrariesActionType,
     quickAlgoLibraryResetAndReloadStateSaga
 } from '../quickalgo_libraries';
-
-const localLanguageStrings = {
-    fr: {
-        label: {
-            print: "écrire",
-            print_end: "écrire %1 en terminant par %2",
-            read: "lire une ligne",
-            readInteger: "lire un entier sur une ligne",
-            readFloat: "lire un nombre à virgule sur une ligne",
-            eof: "fin de la saisie"
-        },
-        code: {
-            print: "print",
-            print_end: "print_end",
-            read: "input",
-            readInteger: "lireEntier",
-            readFloat: "lireDecimal",
-            eof: "finSaisie"
-        },
-        description: {
-            print: "print(texte) affiche le texte sur le terminal",
-            read: "input() retourne une chaîne : la prochaine ligne de l'entrée"
-        },
-        startingBlockName: "Programme",
-        messages: {
-            inputPrompt: "Veuillez écrire une entrée pour le programme.",
-            inputEmpty: "Votre programme a essayé de lire l'entrée alors qu'il n'y avait plus aucune ligne à lire !",
-            outputWrong: "Votre programme n'a pas traité correctement toutes les lignes.",
-            outputCorrect: "Bravo ! Votre programme a traité correctement toutes les lignes.",
-            tooFewChars: "La ligne {0} de la sortie de votre programme est plus courte qu'attendue.",
-            tooManyChars: "La ligne {0} de la sortie de votre programme est plus longue qu'attendue.",
-            tooFewLines: "Trop peu de lignes en sortie",
-            tooManyLines: "Trop de lignes en sortie",
-            correctOutput: "La sortie est correcte !",
-            moreThan100Moves: "La sortie est correcte, mais vous l'avez produite en plus de 100 étapes…"
-        },
-        errorStr: {
-            intro: "La sortie de votre programme est fausse, à la ligne ",
-            expected: " :<br>Attendu: \"<b>",
-            answer: "</b>\",<br>Votre réponse: \"<b>",
-            introChar: "</b>\".<br>(Premier caractère erroné à la colonne ",
-            expectedChar: "; attendu: \"<b>",
-            answerChar: "</b>\", votre réponse: \"<b>"
-        }
-    },
-    de: {
-        label: {
-            print: "schreibe",
-            print_end: "schreibe",
-            read: "lies Zeile",
-            readInteger: "lies Zeile als ganze Zahl",
-            readFloat: "lies Zeile als Komma-Zahl",
-            eof: "Ende der Eingabe",
-            charToNumber: "Zeichen zu Zahl",
-            numberToChar: "Zahl zu Zeichen",
-            charToAscii: "ASCII-Zahl zu Zeichen",
-            asciiToChar: "Zeichen zu ASCII-Zahl",
-        },
-        code: {
-            print: "schreibe",
-            print_end: "schreibe",
-            read: "lies",
-            readInteger: "liesGanzzahl",
-            readFloat: "liesKommazahl",
-            eof: "eingabeEnde",
-            charToNumber: "zeichenZuZahl",
-            numberToChar: "zahlZuZeichen",
-            asciiToChar: "zeichenZuAscii",
-            charToAscii: "asciiZuZeichen",
-        },
-        description: {
-        },
-        startingBlockName: "Programm",
-        messages: {
-            inputPrompt: "Please input a line for the program.", // TODO :: translate two lines
-            inputEmpty: "Your program tried to read the input while there is no line left to read!",
-            outputWrong: "Das Programm hat nicht alle Zeilen richtig ausgegeben.",
-            outputCorrect: "Bravo! Das Programm hat alle Zeilen richtig ausgegeben.",
-            tooFewChars: "Zeile zu kurz: Zeile {0}",
-            tooManyChars: "Zeile zu lang: Zeile {0}",
-            tooFewLines: "Zu wenig Zeilen ausgegeben",
-            tooManyLines: "Zu viele Zeilen ausgegeben",
-            correctOutput: "Die Ausgabe ist richtig!",
-            moreThan100Moves: "Die Ausgabe ist richtig, aber du hast mehr als 100 Schritte benötigt …"
-        },
-        errorStr: {
-            intro: "Das Programm hat nicht alle Zeilen richtig ausgegeben.; in Zeile ",
-            expected: ":<br>Erwartet: \"<b>",
-            answer: "</b>\",<br>deine Ausgabe: \"<b>",
-            introChar: "</b>\".<br>(Erstes falsches Zeichen in Spalte ",
-            expectedChar: "; erwartet: \"<b>",
-            answerChar: "</b>\", deine Ausgabe: \"<b>"
-        }
-    },
-    none: {
-        comment: {
-        }
-    }
-};
 
 export interface SmartContractResultLogLine {
     amount: number,
@@ -149,7 +46,9 @@ export class SmartContractLib extends QuickAlgoLibrary {
     constructor (display, infos) {
         super(display, infos);
 
-        this.setLocalLanguageStrings(localLanguageStrings);
+        this.setLocalLanguageStrings({});
+
+        //TODO: Update concepts
 
         const conceptBaseUrl = (window.location.protocol == 'https:' ? 'https:' : 'http:') + '//'
             + 'static4.castor-informatique.fr/help/printer_codecast.html';
@@ -168,10 +67,6 @@ export class SmartContractLib extends QuickAlgoLibrary {
         this.innerState = {};
     }
 
-    reset(taskInfos, appState: AppStore = null) {
-        this.success = false;
-    };
-
     getInnerState() {
         return this.innerState;
     };
@@ -181,14 +76,7 @@ export class SmartContractLib extends QuickAlgoLibrary {
     }
 
     reloadInnerState(data): void {
-        log.getLogger('smart_contract_lib').debug('RELOADED EVENTS', data, data.initial, data.expectedOutput);
         this.innerState = data;
-    };
-
-    *redrawDisplay() {
-        if (this.display) {
-            log.getLogger('smart_contract_lib').debug('make reset display');
-        }
     };
 
     getComponent() {
