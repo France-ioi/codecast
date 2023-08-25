f3fa59507f1a13f287f51578cb90da6e7167a705
Sébastien Tainon <arkandias1@hotmail.fr>
2023-08-25 18:57:25 +0200

    [Fix] Tralalere documentation

--- frontend/task/documentation/DocumentationMenuCategoryConcept.tsx
+++ frontend/task/documentation/DocumentationMenuCategoryConcept.tsx
@@ -4,6 +4,7 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {DocumentationMenuConcept} from './DocumentationMenuConcept';
 import {faChevronDown} from '@fortawesome/free-solid-svg-icons/faChevronDown';
 import {faChevronUp} from '@fortawesome/free-solid-svg-icons/faChevronUp';
+import {Collapse} from 'react-bootstrap';
 
 export interface DocumentationMenuCategoryConceptProps {
     category: DocumentationConcept,
@@ -12,27 +13,33 @@ export interface DocumentationMenuCategoryConceptProps {
 
 export function DocumentationMenuCategoryConcept(props: DocumentationMenuCategoryConceptProps) {
     const {category, subConcepts} = props;
-    const [expanded, setExpanded] = useState(true);
+    const [expanded, setExpanded] = useState(false);
 
     return (
-        <div className="documentation-tab-left">
-            <div className="documentation-concepts-category-title">
+        <div className={`documentation-tab-left ${expanded ? 'is-expanded' : ''}`}>
+            <div className="documentation-concepts-category-title" onClick={() => setExpanded(!expanded)}>
                 <div className="documentation-concepts-category-title-name">
                     {category.name}
                 </div>
                 <div className="category-fold">
-                    <div className="category-fold-button" onClick={() => setExpanded(!expanded)}>
+                    <div className="category-fold-button">
                         <FontAwesomeIcon icon={expanded ? faChevronUp : faChevronDown}/>
                     </div>
                 </div>
             </div>
 
-            {expanded ? subConcepts.map(concept =>
-                <DocumentationMenuConcept
-                    key={concept.id}
-                    concept={concept}
-                />
-            ) : null}
+            <Collapse in={expanded}>
+                <div>
+                    <div className="documentation-concepts-category-subconcepts">
+                        {subConcepts.map(concept =>
+                            <DocumentationMenuConcept
+                                key={concept.id}
+                                concept={concept}
+                            />
+                        )}
+                    </div>
+                </div>
+            </Collapse>
         </div>
     )
 }
