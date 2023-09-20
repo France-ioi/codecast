5d3864b3af8a56d01f33d524d8031396ab23eb11
Sébastien Tainon <arkandias1@hotmail.fr>
2023-09-20 17:30:59 +0200

    Add smart contract visualization of storage

--- frontend/task/libs/smart_contract/SmartContractViewTransaction.tsx
+++ frontend/task/libs/smart_contract/SmartContractViewTransaction.tsx
@@ -9,6 +9,9 @@ import {convertMichelsonStorageToCodecastFormat} from './smart_contract_utils';
 import {useAppSelector} from '../../../hooks';
 import {AnalysisVariable} from '../../../stepper/analysis/AnalysisVariable';
 import {faChevronDown} from '@fortawesome/free-solid-svg-icons/faChevronDown';
+import {faCheck} from '@fortawesome/free-solid-svg-icons/faCheck';
+import {faTimes} from '@fortawesome/free-solid-svg-icons/faTimes';
+import {Icon} from '@blueprintjs/core';
 
 interface SmartContractViewTransactionProps {
     log: SmartContractResultLogLine,
@@ -24,12 +27,18 @@ export function SmartContractViewTransaction(props: SmartContractViewTransaction
 
     const log = props.log;
     const hasExpansion = undefined !== log.consumed_gas || undefined !== log.paid_storage_size_diff;
+    const transactionStorage = undefined !== log.updated_storage ? log.updated_storage : log.storage;
+    const expectedStorage = undefined !== log.expected?.updated_storage ? log.expected?.updated_storage : log.expected?.storage;
+    const wrongExpectedStorage = props.failed && undefined !== expectedStorage && transactionStorage !== expectedStorage;
 
-    let displayedStorage = undefined !== log.updated_storage ? log.updated_storage : log.storage;
-    if (task.gridInfos.expectedStorage) {
-        const storageVariables = convertMichelsonStorageToCodecastFormat(displayedStorage, task.gridInfos.expectedStorage);
+    const getDisplayedStorage = (storage) => {
+        if (!task.gridInfos.expectedStorage) {
+            return storage;
+        }
 
-        displayedStorage = <div className="scope-function-blocks">
+        const storageVariables = convertMichelsonStorageToCodecastFormat(storage, task.gridInfos.expectedStorage);
+
+        return <div className="scope-function-blocks">
             <ul className='global-scope'>
                 {storageVariables.map((variable, variableIndex) =>
                     <li key={variableIndex}>
@@ -44,36 +53,71 @@ export function SmartContractViewTransaction(props: SmartContractViewTransaction
     }
 
     return (
-        <div className="smart-contract-log">
+        <div className={`smart-contract-log ${props.failed ? 'is-failed' : ''}`}>
             <div className="smart-contract-log__header">
-                <div className="smart-contract-log__date">{DateTime.fromISO(log.date).toLocaleString(DateTime.DATETIME_SHORT)}</div>
-                <div className="smart-contract-log__kind">{capitalizeFirstLetter(log.kind)}</div>
-                <div className="smart-contract-log__hash">{log.operation}</div>
+                <div className="smart-contract-log__icon">
+                    <FontAwesomeIcon icon={props.failed ? faTimes : faCheck}/>
+                </div>
+                {undefined !== log.entrypoint ? <div className="smart-contract-log__entry_point">
+                    {/*<FontAwesomeIcon icon={faArrowRight} className="mr-2"/>*/}
+                    <span>{log.entrypoint}({log.arg})</span>
+                </div> :
+                    <div className="smart-contract-log__kind">{capitalizeFirstLetter(log.kind)}</div>
+                }
+                <div className="smart-contract-log__date">{DateTime.fromISO(log.date).toLocaleString(DateTime.DATETIME_SHORT_WITH_SECONDS)}</div>
             </div>
             <div className="smart-contract-log__body">
                 <div className="smart-contract-log__scalars">
                     <div className="smart-contract-scalar">
-                        <div className="smart-contract-scalar__header">Source</div>
+                        <div className="smart-contract-scalar__header">Caller</div>
                         <div className="smart-contract-scalar__value">{log.as} ({truncateString(log.source, 10)})</div>
                     </div>
                     <div className="smart-contract-scalar">
                         <div className="smart-contract-scalar__header">Amount</div>
-                        <div className="smart-contract-scalar__value">{log.amount}</div>
+                        <div className="smart-contract-scalar__value">{log.amount} tez</div>
                     </div>
+                    {/*<div className="smart-contract-scalar">*/}
+                    {/*    <div className="smart-contract-scalar__header">Balance</div>*/}
+                    {/*    <div className="smart-contract-scalar__value">{log.balance} tez</div>*/}
+                    {/*</div>*/}
+                    {/*<div className="smart-contract-scalar">*/}
+                    {/*    <div className="smart-contract-scalar__header">Level</div>*/}
+                    {/*    <div className="smart-contract-scalar__value">{log.level}</div>*/}
+                    {/*</div>*/}
                 </div>
 
-                {undefined !== log.entrypoint && <div className="smart-contract-log__entry_point">
-                    <FontAwesomeIcon icon={faArrowRight} className="mr-2"/>
-                    <span>{log.entrypoint}({log.arg})</span>
+                {undefined !== log.arg && <div className="smart-contract-scalar mt-2">
+                    <div className="smart-contract-scalar__header">Parameter</div>
+                    <div className="smart-contract-scalar__value">{log.arg}</div>
                 </div>}
 
-                {undefined !== displayedStorage && <div className="smart-contract-log__storage smart-contract-scalar">
+                {/*{wrongExpectedStorage &&*/}
+                {/*    <div className="smart-contract-error mt-2">*/}
+                {/*        <strong>Error, wrong expected storage:</strong>*/}
+                {/*    </div>*/}
+                {/*}*/}
+
+                {undefined !== transactionStorage && <div className={`smart-contract-log__storage smart-contract-scalar ${wrongExpectedStorage ? 'is-wrong' : ''}`}>
                     <div className="smart-contract-scalar__header">
+                        {wrongExpectedStorage && <FontAwesomeIcon icon={faTimes} className="mr-1"/>}
                         {undefined !== log.updated_storage ? 'Updated storage' : 'Storage'}
                         {undefined !== log.storage_size ? ` (${log.storage_size} bits)` : ''}
                     </div>
                     <div className="smart-contract-scalar__value">
-                        {displayedStorage}
+                        {getDisplayedStorage(transactionStorage)}
+                    </div>
+                </div>}
+
+                {wrongExpectedStorage && <div>
+                    <div className="smart-contract-log__storage smart-contract-scalar is-right">
+                        <div className="smart-contract-scalar__header">
+                            <FontAwesomeIcon icon={faCheck} className="mr-1"/>
+                            Expected storage
+                            {undefined !== log.expected.storage_size ? ` (${log.expected.storage_size} bits)` : ''}
+                        </div>
+                        <div className="smart-contract-scalar__value">
+                            <div>{getDisplayedStorage(expectedStorage)}</div>
+                        </div>
                     </div>
                 </div>}
 
