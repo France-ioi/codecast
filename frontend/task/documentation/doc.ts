4da4dbc6715cc816cc12d92508a64083867e888d
Sébastien Tainon <arkandias1@hotmail.fr>
2023-09-27 10:59:18 +0200

    Change link to documentation

--- frontend/task/documentation/doc.ts
+++ frontend/task/documentation/doc.ts
@@ -188,7 +188,7 @@ function getConceptsFromLanguage(hasTaskInstructions: boolean, state: AppStore) 
             'tralalere': 'https://static4.castor-informatique.fr/help/index_tralalere.html',
             'tralalere_en': 'https://static4.castor-informatique.fr/help/index_tralalere_en.html',
             'tralalere_nl': 'https://static4.castor-informatique.fr/help/index_tralalere_nl.html',
-            'tralalere_v2': 'https://static4.castor-informatique.fr/help/index_tralalere_v2.html',
+            'tralalere_v2': 'https://static4.castor-informatique.fr/help/index_tralalere_s3.html',
         };
         let baseConceptUrl = context.infos.conceptViewerBaseUrl;
         if (baseConceptUrl === undefined) {
