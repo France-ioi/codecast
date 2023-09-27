d5b370553e76a4ac601ccccdf62dcb35e6a2bae8
Sébastien Tainon <arkandias1@hotmail.fr>
2023-09-27 12:13:31 +0200

    Add a debug dialog to enable/disable loggers

---
+++ frontend/task/dialog/DebugDialog.tsx
@@ -0,0 +1,53 @@
+import {Checkbox, Dialog, Icon, Radio} from "@blueprintjs/core";
+import React, {useCallback, useEffect, useState} from "react";
+import log from 'loglevel';
+
+export function DebugDialog() {
+    const [isOpen, setOpen] = useState(false);
+    const [refreshTrigger, setRefreshTrigger] = useState(0);
+
+    let lastTyped = [];
+    const debugCode = "17931793";
+
+    const handleUserKeyPress = useCallback(e => {
+        lastTyped.push(e.key);
+        lastTyped = lastTyped.slice(Math.max(0, lastTyped.length - debugCode.length), lastTyped.length);
+        if (debugCode === lastTyped.join('')) {
+            setOpen(true);
+        }
+    }, []);
+
+    const onChangeLevel = useCallback((name: string, enabled) => {
+        log.getLogger(name).setLevel(enabled ? 1 : 2);
+        setRefreshTrigger(ref => ref + 1);
+    }, []);
+
+    useEffect(() => {
+        window.addEventListener('keypress', handleUserKeyPress);
+
+        return () => {
+            window.removeEventListener("keydown", handleUserKeyPress);
+        };
+    }, [handleUserKeyPress]);
+
+    return (
+        <Dialog
+            isOpen={isOpen}
+            className={`simple-dialog`}
+            canOutsideClickClose
+            canEscapeKeyClose
+            onClose={() => setOpen(false)}
+        >
+            <div>
+                <h1>Choose which log levels to enable:</h1>
+
+                {Object.entries(log.getLoggers()).map(([name, logger]) =>
+                    <div style={{display: 'flex'}} key={name}>
+                        <span className="mr-2">{name}</span>
+                        <Checkbox checked={logger.getLevel() === 1} onChange={() => onChangeLevel(name, logger.getLevel() !== 1)}/>
+                    </div>
+                )}
+            </div>
+        </Dialog>
+    );
+}
