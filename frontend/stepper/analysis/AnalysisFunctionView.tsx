b8b735050cc3b14223fbc39fa85a83dfdf17d27a
Sébastien Tainon <arkandias1@hotmail.fr>
2023-11-22 17:20:51 +0100

    Remove parenthesis which are given in C++ but not in Python

--- frontend/stepper/analysis/AnalysisFunctionView.tsx
+++ frontend/stepper/analysis/AnalysisFunctionView.tsx
@@ -12,7 +12,7 @@ const FILTERED_NAMES = [
 ];
 
 export const AnalysisFunctionView = (props: AnalysisFunctionViewProps): JSX.Element => {
-    const stackFrameName = props.stackFrame.name && -1 === FILTERED_NAMES.indexOf(props.stackFrame.name) ? props.stackFrame.name : null;
+    const stackFrameName = props.stackFrame.name && -1 === FILTERED_NAMES.indexOf(props.stackFrame.name) ? props.stackFrame.name.replace(/\(\)/g, '') : null;
 
     return (
         <div className="stack-frame stack-frame-focused">
