2d4648c7f977d07c4a6c00415eb525c2630d3795
Sébastien Tainon <arkandias1@hotmail.fr>
2023-11-24 18:29:47 +0100

    Backward compatibility for block answer

--- frontend/stepper/compile.ts
+++ frontend/stepper/compile.ts
@@ -33,7 +33,7 @@ import {appSelect} from '../hooks';
 import {LibraryTestResult} from '../task/libs/library_test_result';
 import {CodecastPlatform} from './codecast_platform';
 import {App, Codecast} from '../app_types';
-import {documentToString, TextBufferHandler} from '../buffers/document';
+import {BlockBufferHandler, documentToString, TextBufferHandler} from '../buffers/document';
 import {selectActiveBufferPlatform} from '../buffers/buffer_selectors';
 import {TaskAnswer} from '../task/task_types';
 import {RECORDING_FORMAT_VERSION} from '../version';
@@ -175,6 +175,12 @@ export default function(bundle: Bundle) {
                 const platform = yield* appSelect(state => state.options.platform);
                 answer = {
                     document: TextBufferHandler.documentFromString(answer),
+                    platform,
+                };
+            } else if ('object' === typeof answer && answer.blockly) {
+                const platform = yield* appSelect(state => state.options.platform);
+                answer = {
+                    document: BlockBufferHandler.documentFromObject(answer),
                     platform,
                 };
             }
