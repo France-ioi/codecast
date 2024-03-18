2f7a862a8b0ee46a199c69012f268039e5600d15
Sébastien Tainon <arkandias1@hotmail.fr>
2024-03-18 18:42:05 +0100

    Translate in french for now

--- frontend/buffers/html/html_editor_config.ts
+++ frontend/buffers/html/html_editor_config.ts
@@ -6,7 +6,7 @@ export interface ToolboxCategoryBlocks {
     id: number,
     tag: string,
     paired: boolean,
-    desc: String,
+    desc?: string,
 }
 
 export interface ToolboxCategory {
@@ -54,137 +54,122 @@ export const tbConf: ToolboxConfiguration = {
     categories: [
         {
             id: 1,
-            name: 'Blocks',
+            name: 'Blocs',
             highlight: '#4a90e2',
             blocks: [
                 {
                     id: 1,
                     tag: 'div',
                     paired: true,
-                    desc: 'This is a div element'
                 },
                 {
                     id: 2,
                     tag: 'h1',
                     paired: true,
-                    desc: 'This is an h1 element'
                 },
                 {
                     id: 3,
                     tag: 'p',
                     paired: true,
-                    desc: 'This is a p element'
+                    desc: 'Un paragraphe'
                 },
                 {
                     id: 4,
                     tag: 'ul',
                     paired: true,
-                    desc: 'This is a ul element'
+                    desc: "Une liste d'items",
                 },
                 {
                     id: 5,
                     tag: 'li',
                     paired: true,
-                    desc: 'This is an li element'
+                    desc: "Un item d'une liste",
                 }
             ]
         },
         {
             id: 2,
-            name: 'Forms',
+            name: 'Formulaires',
             highlight: '#b8e986',
             blocks: [
                 {
                     id: 6,
                     tag: 'form',
                     paired: true,
-                    desc: 'This is a form tag'
                 },
                 {
                     id: 7,
                     tag: 'input',
                     paired: false,
-                    desc: 'This is an input tag'
                 },
                 {
                     id: 8,
                     tag: 'label',
                     paired: true,
-                    desc: 'This is a label tag'
                 },
                 {
                     id: 9,
                     tag: 'select',
                     paired: true,
-                    desc: 'This is a select tag'
                 },
                 {
                     id: 10,
                     tag: 'button',
                     paired: true,
-                    desc: 'This is a button tag'
                 },
                 {
                     id: 11,
                     tag: 'textarea',
                     paired: true,
-                    desc: 'This is a textarea tag'
                 }
             ]
         },
         {
             id: 3,
-            name: 'Inline',
+            name: 'En ligne',
             highlight: '#4a51e2',
             blocks: [
                 {
                     id: 12,
                     tag: 'span',
                     paired: true,
-                    desc: 'This is a span tag'
                 },
                 {
                     id: 13,
                     tag: 'i',
                     paired: true,
-                    desc: 'This is an i tag'
                 },
                 {
                     id: 14,
                     tag: 'b',
                     paired: true,
-                    desc: 'This is a b tag'
                 }
             ]
         },
         {
             id: 4,
-            name: 'Miscellaneous',
+            name: 'Divers',
             highlight: '#c346f0',
             blocks: [
                 {
                     id: 15,
                     tag: 'script',
                     paired: true,
-                    desc: 'This is a script tag'
                 },
                 {
                     id: 16,
                     tag: 'article',
                     paired: true,
-                    desc: 'This is an article tag'
                 },
                 {
                     id: 17,
                     tag: 'canvas',
                     paired: true,
-                    desc: 'This is a canvas tag'
                 },
                 {
                     id: 18,
                     tag: 'nav',
                     paired: true,
-                    desc: 'This is a nav tag'
                 }
             ]
         }
