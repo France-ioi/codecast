6982cf100c820ab7488ca9302af6933a5c7c2c76
Sébastien Tainon <arkandias1@hotmail.fr>
2024-08-28 15:48:57 +0200

    Exclude defined variables

--- frontend/stepper/python/analysis.ts
+++ frontend/stepper/python/analysis.ts
@@ -14,7 +14,7 @@ import log from 'loglevel';
  */
 const SKULPT_ANALYSIS_DEBUG = 1;
 
-export const convertSkulptStateToAnalysisSnapshot = function (suspensions: readonly any[], lastAnalysis: AnalysisSnapshot, newStepNum: number): AnalysisSnapshot {
+export const convertSkulptStateToAnalysisSnapshot = function (suspensions: readonly any[], lastAnalysis: AnalysisSnapshot, newStepNum: number, excludedVariableNames: string[]): AnalysisSnapshot {
     // @ts-ignore
     if (SKULPT_ANALYSIS_DEBUG === 2) {
         log.getLogger('python_runner').debug('[¥¥¥¥¥¥¥] Building analysis');
@@ -57,7 +57,7 @@ export const convertSkulptStateToAnalysisSnapshot = function (suspensions: reado
 
             log.getLogger('python_runner').debug('suspension loaded', suspension.$loaded_references);
 
-            const scope = analyseSkulptScope(suspension);
+            const scope = analyseSkulptScope(suspension, excludedVariableNames);
             stackFrame.scopes.push(scope);
             stackFrame.name = scope.name;
             stackFrames.push(stackFrame);
@@ -95,7 +95,7 @@ const isProgramSuspension = function(suspension): boolean {
 /**
  * Transforms the skulpt scope (one suspension) to something readable with the variables content.
  */
-export const analyseSkulptScope = function(suspension: any): AnalysisScope {
+export const analyseSkulptScope = function(suspension: any, excludedVariableNames: string[]): AnalysisScope {
     // @ts-ignore
     if (SKULPT_ANALYSIS_DEBUG === 2) {
         log.getLogger('python_runner').debug('////// Analyse scope...');
@@ -118,7 +118,7 @@ export const analyseSkulptScope = function(suspension: any): AnalysisScope {
         suspVariables = suspension.$loc;
     }
 
-    const variableNames = sortArgumentsFirst(filterInternalVariables(Object.keys(suspVariables)), suspension._argnames);
+    const variableNames = sortArgumentsFirst(filterInternalVariables(Object.keys(suspVariables), excludedVariableNames), suspension._argnames);
     const loadedReferences = suspension.$loaded_references ? suspension.$loaded_references : {};
 
     for (let variableName of variableNames) {
@@ -188,9 +188,12 @@ const variablesBeginWithIgnore = [
  *
  * @returns {Array}
  */
-const filterInternalVariables = (variableNames: string[]): string[] => {
+const filterInternalVariables = (variableNames: string[], excludedVariableNames: string[]): string[] => {
     return variableNames.filter((name) => {
-        let ignore = false;
+        if (-1 !== excludedVariableNames.indexOf(name)) {
+            return false;
+        }
+
         for (let variableBeginWithIgnore of variablesBeginWithIgnore) {
             if (name.indexOf(variableBeginWithIgnore) === 0) {
                 return false;
