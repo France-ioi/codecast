7cb92208f974503c89f0469448f0fed4ccdb5e02
Sébastien Tainon <arkandias1@hotmail.fr>
2024-09-30 10:52:47 +0200

    Add text length to notions so that when it is enabled it activates both Blockly and Python blocks

--- frontend/task/blocks/notions.ts
+++ frontend/task/blocks/notions.ts
@@ -50,6 +50,9 @@ export const defaultNotions: NotionArborescence = {
         'setattr',
         'lambda',
     ],
+    'texts': [
+        'text_length',
+    ],
     'variables': [
         'variables_set',
     ],
