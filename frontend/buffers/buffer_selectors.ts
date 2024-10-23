83265fa5d1f072cbfb25e6d15b92d97195b450a1
Sébastien Tainon <arkandias1@hotmail.fr>
2024-10-23 12:44:29 +0200

    Memoize buffer selector for more efficiency

--- frontend/buffers/buffer_selectors.ts
+++ frontend/buffers/buffer_selectors.ts
@@ -2,12 +2,13 @@ import {AppStore} from '../store';
 import {BufferState} from './buffer_types';
 import {CodecastPlatform} from '../stepper/codecast_platform';
 import {BuffersState} from './buffers_slice';
+import {memoize} from 'proxy-memoize';
 
 export function selectSourceBuffers(state: AppStore): {[bufferName: string]: BufferState} {
     return selectSourceBuffersFromBufferState(state.buffers);
 }
 
-export function selectSourceBuffersFromBufferState(buffers: BuffersState): {[bufferName: string]: BufferState} {
+export const selectSourceBuffersFromBufferState = memoize((buffers: BuffersState): {[bufferName: string]: BufferState} => {
     const sourceBuffers = {};
     for (let [bufferName, buffer] of Object.entries(buffers.buffers)) {
         if (buffer.source) {
@@ -16,13 +17,20 @@ export function selectSourceBuffersFromBufferState(buffers: BuffersState): {[buf
     }
 
     return sourceBuffers;
-}
+});
 
 export function selectActiveBufferPlatform(state: AppStore): CodecastPlatform {
     if (null === state.buffers.activeBufferName) {
         return state.options.platform;
     }
 
-
     return state.buffers.buffers[state.buffers.activeBufferName].platform;
+}
+
+export function selectActiveBuffer(state: AppStore): BufferState|null {
+    if (null === state.buffers.activeBufferName) {
+        return null;
+    }
+
+    return state.buffers.buffers[state.buffers.activeBufferName];
 }
